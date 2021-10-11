a2f92c8114237877ed245c836a925b3d05e476f0
Aditya Jadhav <adi30jadhav@gmail.com>
2021-10-10 20:56:06 -0400

    Added Valgrind Outputs

--- app/main.cpp
+++ app/main.cpp
@@ -1,13 +1,22 @@
-#include <iostream>
-#include <AnalogSensor.hpp>
+/**
+ * @file main.cpp
+ * @author Aditya Jadhav (amjadhav@umd.edu)
+ * @brief This program reads the analog values of the sensor
+ * @version 0.1
+ * @date 2021-10-08
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
-int main()
-{
+#include <iostream>
+#include "../include/AnalogSensor.hpp"
+
+int main() {
     AnalogSensor lightSensor(5);
     std::cout << "Averaged sensor reading: " << lightSensor.Read() << std::endl;
-    bool terminator;
-    if( terminator )
-    {
+    bool terminator = true;
+    if (terminator) {
         std::cout << "DONE" << std::endl;
     }
     return 0;
--- include/AnalogSensor.hpp
+++ include/AnalogSensor.hpp
@@ -1,13 +1,23 @@
+/**
+ * @file AnalogSensor.hpp
+ * @author Aditya Jadhav (amjadhav@umd.edu)
+ * @brief Class AnalogSensor declaration
+ * @version 0.1
+ * @date 2021-10-08
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 #pragma once
 
 #include <iostream>
 
-class AnalogSensor
-{
-public:
-    AnalogSensor(unsigned int samples);
-    ~AnalogSensor();
-    int Read();
-private:
+class AnalogSensor {
+ public:
+  explicit AnalogSensor(unsigned int samples);
+  ~AnalogSensor();
+  int Read();
+ private:
     unsigned int mSamples;
 };
